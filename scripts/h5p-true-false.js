ee7ca402987365b2d43c18274dc20b4dc72acb88
SimenBergo <bergo.simen@gmail.com>
2026-08-18 13:22:44 +0200

    Trigger CI run #2

--- scripts/h5p-true-false.js
+++ scripts/h5p-true-false.js
@@ -38,6 +38,7 @@ H5P.TrueFalse = (function ($, Question) {
   function TrueFalse(options, id, contentData) {
     var self = this;
 
+    
     // Inheritance
     Question.call(self, 'true-false', { theme: true });
 
